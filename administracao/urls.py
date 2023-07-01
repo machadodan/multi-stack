3cffb4d7eb0aabc4368dba6cd6238cc7566ba423
machadodan <danielg_m@hotmail.com>
2023-07-01 15:59:47 -0300

    atualizacao projeto para deploy

---
+++ administracao/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from django.urls.base import reverse_lazy
+from .views import diaria_view, servico_view, usuario_view
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('diarias/listar', diaria_view.lista_diarias, name='listar_diarias'),   
+    path('diarias/<int:diaria_id>/transferir', diaria_view.transferir_pagamento_diaria,
+    name='transferir_pagamento'),
+    path('servicos/cadastrar', servico_view.cadastrar_servico, name='cadastrar_servico'),
+    path('servicos/listar', servico_view.listar_servicos, name='listar_servicos'),
+    path('servicos/editar/<int:id>', servico_view.editar_servico, name='editar_servico'),
+    path('usuarios/cadastrar', usuario_view.cadastrar_usuario, name='cadastrar_usuario'),
+    path('usuarios/listar', usuario_view.listar_usuarios, name='listar_usuarios'),
+    path('usuarios/editar/<int:id>', usuario_view.editar_usuario, name='editar_usuario'),
+    path('autenticacao/login', auth_views.LoginView.as_view(), name='logar_usuario'),
+    path('autenticacao/logout', auth_views.LogoutView.as_view(), name='deslogar_usuario'),
+    path('alterar_senha', auth_views.PasswordChangeView.as_view(
+        success_url=reverse_lazy('listar_servicos')), name='alterar_senha'),
+    path('resetar_senha', auth_views.PasswordResetView.as_view(), name='resetar_senha'),
+    path('resetar_senha/sucesso', auth_views.PasswordResetDoneView.as_view(), 
+        name='resetar_senha_sucesso'),
+    path('resetar_senha/<str:uidb64>/<str:token>', auth_views.PasswordResetConfirmView.as_view(), 
+    name='resetar_senha_confirmar'),
+    path('resetar_senha/feito', auth_views.PasswordResetDoneView.as_view(), 
+    name='resetar_senha_feito'),
+]
---
+++ api/services/usuario_service.py
@@ -0,0 +1,11 @@
+from ..models import Usuario
+
+def listar_usuario_email(email):
+    return Usuario.object.get(email=email)
+
+def listar_usuario_id(id):
+    return Usuario.object.get(id=id)
+
+def atualizar_reputacao_usuario(usuario, reputacao):
+    usuario.reputacao = reputacao
+    usuario.save()
